958bb77659249f8ab7897bd51a0ad1abbe7bd335
Rory.Phan <rory.phan@avepoint.com>
2023-10-26 10:58:12 +0700

    nestjs template has auth and user

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
+import { UsersService } from '../users/users.service';
+import { usersProviders } from '../users/users.providers';
+
+@Module({
+  imports: [
+    PassportModule,
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWTKEY,
+      signOptions: { expiresIn: process.env.TOKEN_EXPIRATION },
+    }),
+  ],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    UsersService,
+    ...usersProviders,
+  ],
+  controllers: [AuthController],
+})
+export class AuthModule {}
